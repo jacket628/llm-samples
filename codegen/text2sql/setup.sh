a1fd8e06f5cbdd7003225608b88e7a6d95f6a011
jack yu <275211360@qq.com>
2023-11-30 17:51:36 +0800

    second updated

--- codegen/text2sql/setup.sh
+++ codegen/text2sql/setup.sh
@@ -4,10 +4,10 @@
 aws rds create-db-subnet-group \
     --db-subnet-group-name mydbsubnetgroup \
     --db-subnet-group-description "My DB subnet group" \
-    --subnet-ids subnet-0b2a1b5b6515c7907 subnet-0f9d912306efdd1ff
+    --subnet-ids subnet-062ce5bd54ff259a1 subnet-0ead854a03c3ba428 
 
 # 创建 RDS MySQL 数据库实例,命名为 mydb,db.t2.micro 类型,用户名 admin,密码为 abc123
-aws rds create-db-instance --db-instance-identifier llmnew2 --engine mysql --db-instance-class db.r6g.large --master-username admin --master-user-password admin12345678 --allocated-storage 100 --db-subnet-group-name mydbsubnetgroup --vpc-security-group-ids sg-048fb75fba803e623
+aws rds create-db-instance --db-instance-identifier llmnew2 --engine mysql --db-instance-class db.r6g.large --master-username admin --master-user-password admin12345678 --allocated-storage 100 --db-subnet-group-name mydbsubnetgroup --vpc-security-group-ids sg-09f8174fdc789f38b
 # 等待数据库实例创建完成并可用
 aws rds wait db-instance-available --db-instance-identifier llmnew2
 endpoint=$(aws rds describe-db-instances --db-instance-identifier llmnew2 --query 'DBInstances[*].Endpoint.Address' --output text)
